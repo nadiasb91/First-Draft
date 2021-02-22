7ff6c3447958aef30865c0d18781cc6cde5c87de
nadiasb91 <nadiasb91@gmail.com>
2021-02-22 12:28:34 -0300

    delete import that are not been used

--- src/test/java/Pages/MyAccountPage.java
+++ src/test/java/Pages/MyAccountPage.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.sql.Time;
-import java.util.concurrent.TimeUnit;
 
 public class MyAccountPage {
     WebDriver driver;
